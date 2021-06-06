3ca9cea9ccabad1179bae1dad7997bdccb201e8a
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-06-05 23:21:07 -0500

    fadein animation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { ListCategories } from './components/ListCategories'
 import { ListPhotoCards } from './components/ListPhotoCards'
 import Logo from './components/Logo'
-import {GlobalStyle}from './GlobalStyle'
+import { GlobalStyle } from './styles/GlobalStyle'
+
 export const App = ()=>{
   return(
     <>
