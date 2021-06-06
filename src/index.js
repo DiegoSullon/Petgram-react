8dd7a076ad107f1870381b4dbafccef36e41ad92
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-06-05 21:27:14 -0500

    category styled-components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './app'
 
-ReactDOM.render(<h1>Hola Platzi</h1>,
+ReactDOM.render(<App/>,
   document.getElementById('app'))
