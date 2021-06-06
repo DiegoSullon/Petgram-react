55ce6997908491c71ab7e8be55c9c04ffa4c1d02
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-06-05 23:08:08 -0500

    logo svg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ListCategories } from './components/ListCategories'
 import { ListPhotoCards } from './components/ListPhotoCards'
+import Logo from './components/Logo'
 import {GlobalStyle}from './GlobalStyle'
 export const App = ()=>{
   return(
     <>
+    <Logo/>
     <GlobalStyle/>
     <ListCategories />
     <ListPhotoCards/>
