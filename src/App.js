e3c02ab49d80bbf19972dbb42dcf9bad2855cdd2
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-06-05 21:57:08 -0500

    GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { ListCategories } from './components/ListCategories'
-
+import {GlobalStyle}from './GlobalStyle'
 export const App = ()=>{
   return(
+    <>
+    <GlobalStyle/>
     <ListCategories />
+    </>
   )
 }
 
