156f74bf5ddf23953f1df65a1c83f6a1bef84770
DiegoSullon <jdiego.sullon@gmail.com>
2021-10-04 20:09:57 -0500

    webpack conf

--- src/components/ListCategories/index.js
+++ src/components/ListCategories/index.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
-import { categories } from '../../../api/db.json'
+
 export const ListCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    window.fetch('https://petgram-server-sullon.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
   return (
     <List>
-      {
-        categories.map((c) =>
-          <Item key={c.id}><Category {...c} /></Item>
-        )
-      }
+      {categories.map(c => (
+        <Item key={c.id}>
+          <Category {...c} />
+        </Item>
+      ))}
     </List>
   )
 }
