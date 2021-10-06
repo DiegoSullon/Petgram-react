6901c0bb12eb79a8842c20116daa543c2b9c37fb
DiegoSullon <jdiego.sullon@gmail.com>
2021-10-06 18:03:21 -0500

    scroll effect list of categories

--- src/components/ListCategories/index.js
+++ src/components/ListCategories/index.js
@@ -4,20 +4,39 @@ import { Item, List } from './styles'
 
 export const ListCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
   useEffect(() => {
-    window.fetch('https://petgram-server-sullon.vercel.app/categories')
+    window
+      .fetch('https://petgram-server-sullon.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
       })
   }, [])
-  return (
-    <List>
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = fixed => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map(c => (
         <Item key={c.id}>
           <Category {...c} />
         </Item>
       ))}
     </List>
+  )
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
